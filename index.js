4f43b8ceea13c5d189e4029ca5abaa35543e9d45
Threstle <Etienne.Chaumont@gmail.com>
2015-03-10 20:01:44 +0100

    first commit

---
+++ index.js
@@ -0,0 +1,11 @@
+/**
+ * Escape special characters in the given string of html.
+ *
+ * @param  {String} html
+ * @return {String}
+ */
+module.exports = {
+  glitchFile: function(file) {
+    console.log('glitch test');
+  }
+};
