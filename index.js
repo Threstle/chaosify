c425bb8929e6d7c1e433600179061815b5c10e83
Lama <Etienne.Chaumont@gmail.com>
2015-03-11 17:16:04 +0100

    changes'

--- index.js
+++ index.js
@@ -6,7 +6,7 @@
  */  
 
 var fs = require('fs');
-
+var readimage = require('readimage');
 module.exports = {
 	glitchFile: function(path,finalPath,intensity) {
 	    fs.readFile(path, function(err,data){
@@ -20,14 +20,22 @@ module.exports = {
 	    strData = strData.substr(100, strData.length);
 	    console.log(data);
 	    //data = strData
-	    var header = 6;
+	   // var header = data.slice(0,data.length/10);
+	   // data = data.slice(data.length/10, data.length);
+	   intensity = 40;
 	    for(var i = 0; i < intensity; i++){
-	    	data = module.exports.randomizeChar(data,data.length,data.length/10,5000);
-	    	data = module.exports.copyChar(data,data.length,data.length/10,5000,1);
+	    	data = module.exports.copyChar(data,2,5000,1);
+	    	data = module.exports.randomizeChar(data,11,5000);
+	    	//data = module.exports.invertChar(data,101);
+	    	//data = module.exports.skipChar(data,100);
+	    	
 		}
+		//data = header.concat(data);
+		//data = Buffer.concat([header, data]);
+		 console.log(data);
   		fs.writeFile(finalPath, data, function (err) {
 		  if (err) throw err;
-		  console.log('It\'s saved!');
+		  console.log('It\'s savesed!');
 		});
 	  })
 
@@ -35,8 +43,9 @@ module.exports = {
 
 	randomizeChar: function(data,nbIt,sizeHeader,rand){
 	  	for(var i = 0; i < nbIt; i++){
-		    var i = Math.floor(data.length*Math.random()+sizeHeader);
-	  		data[i] = Math.floor(Math.random()*rand);
+		    var k = Math.floor(data.length*Math.random()+sizeHeader);
+	  		data[k] = Math.floor(Math.random()*rand);
+	  		
 	    }
 
 	  	return data;
@@ -44,11 +53,30 @@ module.exports = {
 
 	copyChar: function(data,nbIt,sizeHeader,rand,sizeDecal){
 		for(var i = 0; i < nbIt; i++){
-			var i = Math.floor(data.length*Math.random()+sizeHeader);
+			var k= Math.floor(data.length*Math.random()+sizeHeader);
 			for(var j = 0; j < sizeDecal; j++){
 				data[i+j+sizeDecal] = data[i+j];
 			}
 		}
+		return data;
+	},
+
+	invertChar: function(data,sizeHeader){
+
+		for(var j = sizeHeader+1000; j < data.length-1000; j++){
+			if(data[j] == 1)data[j] = 0;
+			if(data[j] == 0)data[j] = 1;
+		}
+		
+		return data;
+	},
+
+	skipChar: function(data,sizeHeader){
+
+		for(var j = sizeHeader; j < data.length-500; j+=20){
+			data[j] = "66";
+		}
+		
 		return data;
 	}
 
--- test.js
+++ test.js
@@ -1,3 +1,3 @@
 ch = require('./index.js');
 
-ch.glitchFile('./imgTest/test.jpg','./imgTest/test_glitched.jpg',1);
+ch.glitchFile('./imgTest/test_2.jpg','./imgTest/test_glitched.jpg',1);
